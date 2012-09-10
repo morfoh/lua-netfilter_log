dbf8d49b8e17e7897e6d52cfa1c25ab2ee04695f
Robert G. Jakabosky <bobby@sharedrealm.com>
2012-09-10 00:01:10 -0700

    Add test script.

---
+++ test.lua
@@ -0,0 +1,37 @@
+
+local nf_log = require"netfilter_log"
+
+local h = nf_log.nflog();
+
+print("nflog_unbind:",h:unbind_pf(nf_log.AF_INET))
+
+print("nflog_bind:",h:bind_pf(nf_log.AF_INET))
+
+local gh = nf_log.nflog_group(h, 0)
+print("gh = ", gh)
+local gh100 = nf_log.nflog_group(h, 100)
+print("gh100 = ", gh100)
+
+print("nflog_set_mode:",gh:set_mode(nf_log.NFULNL_COPY_PACKET, 0xffff))
+
+local fd = h:fd()
+print("fd = ", fd)
+
+print("callback = ", gh:callback_register(function(...)
+	print("nflog_callback():", ...)
+	return 0
+end))
+
+-- main loop
+---[[
+repeat
+ local rc = h:handle_packet()
+ print("handle_packet(): rc=", rc)
+until rc < 0
+--]]
+
+print("nflog_unbind from group 0:",gh:unbind())
+print("nflog_unbind from group 100:",gh100:unbind())
+
+print("nflog_closre:", h:close())
+
